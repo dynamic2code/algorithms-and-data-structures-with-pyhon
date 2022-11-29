a23aa1a49c16d8fc57eb7c99e3871fe87cf59218
Joseph Wakahu Ngacha <83699448+dynamic2code@users.noreply.github.com>
2022-11-29 14:13:11 +0300

    working with Lists in python

---
+++ lists.py
@@ -0,0 +1,25 @@
+#the file is to show how to work on lists
+
+#creating a list 
+
+class MyList:
+    def __init__(self):
+        self.names = []
+
+    def addToList(self):
+        #the lst will take inputs from user 
+        userInPut = input("enter your inputs separated by a whitespace: ")
+        self.names.extend(userInPut.split())
+        print("The new list is: \n {}".format(self.names))
+    
+    def removeFromList(self):
+        removeWithIndex = int(input("Enter the position you want removed: "))
+        removeWithItemName = input("Enter the name of the item you wish removed: ")
+
+        self.names.pop(removeWithIndex)
+        self.names.remove(removeWithItemName)
+
+if __name__ == "__main__":
+    myList = MyList()
+    myList.addToList()
+    myList.removeFromList()
