7c9e7722490321deb74a820b9f2e4e1695ab9825
Joseph Wakahu Ngacha <83699448+dynamic2code@users.noreply.github.com>
2022-11-30 06:46:50 +0300

    working with dictionaries python

---
+++ dictionaries.py
@@ -0,0 +1,19 @@
+#ths file shows how to work with python dictionaries
+
+class Dictionary:
+    def __init__(self):
+        # creating an empty dctionary
+        self.dictionary = {}
+    
+    def addToDictionary(self):
+        # user inputs the values
+        values = input("")
+    
+    def accecingDictionaryElement():
+        pass
+
+    def removeFromDictionary(self):
+        pass
+
+    def deleteDictionary(self):
+        pass
