dc88ccc7a6c74bc4d8e8d252597c15951afcf2f9
Joseph Wakahu Ngacha <83699448+dynamic2code@users.noreply.github.com>
2022-12-20 21:11:28 +0300

    improvement on set methods

--- sets.py
+++ sets.py
@@ -2,20 +2,27 @@
 
 class MySet:
     def __init__(self):
-        self.set = {}
+        self._set = {}
 
     def addToSet(self):
         elements  = input("enter the elements: ")
-        self.set.add(elements)
+        self._set.add(elements)
 
-    def accesingSetElements(self):
-        pass
+    def serchingSetElements(self):
+        want = input('enter the element you wish to search: ')
+        if want in self._set:
+            print ('the element you searched s precent')
 
-    def removingFromSet(self):
-        pass
 
     def deleteFromSet(self):
-        pass
+        desition = int(input('Enter \n 1 to delete the whole set \n 2 to delete an item: '))
+
+        if desition == 1 :
+            self._set.clear()
+        elif desition == 2:
+            print(self._set)
+            d2 = int(input('Enter the index you wish to remove: '))
+            self._set.pop(d2)
 
 
 if __name__ == "__main__":
