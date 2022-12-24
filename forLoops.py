49dec0055d812f65a7a191857de052309fce794d
Joseph Wakahu Ngacha <83699448+dynamic2code@users.noreply.github.com>
2022-12-24 23:02:37 +0300

    for loop for list

---
+++ forLoops.py
@@ -0,0 +1,10 @@
+#looping over data structures
+
+class ForLoops:
+    def __init__(self):
+        self.list1 =[[2,1,3,56],"man",['duck','pie','chiken']]
+
+    def loopignList(self):
+    #loopign through a list
+        for i in self.list1:
+            print(i)
